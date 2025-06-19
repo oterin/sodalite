0509be1bf87497b0aec444092f07f8291ddac721
Milan Oterin <milanoterin@gmail.com>
2025-06-19 22:45:31 +0300

    Initialize Next.js frontend with Tailwind and UI components

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Toaster } from "sonner";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "sodalite - a simple downloader for the web",
+  description: "no ads, no tracking, just downloads",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {children}
+        <Toaster position="bottom-center" theme="system" richColors />
+      </body>
+    </html>
+  );
+}

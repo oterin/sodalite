a9946f43a97e856b86ac9a145bd76ba776a5b68e
Milan Oterin <milanoterin@gmail.com>
2025-06-19 23:54:49 +0300

    I forgot to add service detection :P silly me

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -1,6 +1,22 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function detectService(url: string): string {
+  if (
+    url.includes("instagram.com/reel") ||
+    url.includes("instagram.com/reels")
+  ) {
+    return "Instagram Reels";
+  }
+  if (url.includes("youtube.com") || url.includes("youtu.be")) {
+    return "YouTube";
+  }
+  if (url.includes("tiktok.com")) {
+    return "TikTok";
+  }
+  return "";
 }

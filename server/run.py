6f05c01c2c1ddbf6057e9dd2f4f63a609807db6a
Milan Oterin <milanoterin@gmail.com>
2025-06-20 05:03:32 +0300

    Enable SSL support for HTTPS
    Use environment variables for SSL key and certificate file paths. Also
    load environment variables from server.env.

--- server/run.py
+++ server/run.py
@@ -3,12 +3,18 @@ development server runner for sodalite
 """
 
 import uvicorn
+import os
+from dotenv import load_dotenv
 
 if __name__ == "__main__":
+    load_dotenv("server.env")
+
     uvicorn.run(
         "server.main:app",
         host="0.0.0.0",
         port=1337,
         reload=True,
-        log_level="info"
+        log_level="info",
+        ssl_keyfile=os.getenv("SSL_KEYFILE"),
+        ssl_certfile=os.getenv("SSL_CERTFILE")
     )
